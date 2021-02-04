5219de2d40e415e21cc5f08d7dc1325c2f66b985
Johnatan Andrés Arango R <ingyotis@gmail.com>
2021-02-04 00:10:52 -0500

    starting the project, adding devServer and the neccesary files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//requires
+import './styles.css'
+
+//constantes
+const API_KEY = '1398e87212256f8dbf6f3e7deed06876'
+const ENTRY_POINT = 'http://api.openweathermap.org/data/2.5/'
+
+console.log('hello world')
+
